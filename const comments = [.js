13dc9ae391c4e95f1f85b9b9bc21f85c36cf9fff
Mrinalsharma1 <checkkro1@gmail.com>
2025-04-27 15:59:39 +0530

    [CU-86cyqdx43] Updated parent POM to use stable Spring Boot 3.2

---
+++ const comments = [.js
@@ -0,0 +1,14 @@
+const comments = [
+    {
+        id: 1,
+        title: "Because of this course I was able to clear my two interviews... Thanks for making such wonderful content.",
+        profileImage: "",
+        profileimage: ""
+    },
+    {
+        id: 1,
+        title: "lorem",
+        profileImage: "",
+        profileimage: ""
+    }
+]
